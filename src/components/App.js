c6201310dccbb70766abbda56fc931bfe5caf88b
denishrana09 <denishrana09@gmail.com>
2019-03-15 09:38:29 +0530

    fix: mini-css-extract-plugin

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
 import Icon from '../images/icon.png';
 
 class App extends Component {
   render() {
+    // console.log('7 ', process.env.NODE_ENV);
     return (
       <div>
         <h1>React App without <a href="https://github.com/facebook/create-react-app">CRA</a></h1>
-        <div className="temp">Temp Text</div>
+        <div className="temp">Hello there</div>
         <div className="t1">
           <div className="t2">
-            <h4>Another Temp text!!</h4>
+            <h4>How you doin</h4>
           </div>
         </div>
         <img src={Icon}></img>
+        <Nav />
       </div>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../nav.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <div className="nav-css">lorem ipsum</div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
